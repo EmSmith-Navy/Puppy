15a480e9252c132a4eb9c15358529d4503903766
EmSmith-Navy <keino2586@gmail.com>
2024-10-28 11:34:56 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import "./App.css";
+import { useState } from "react";
+import { puppyList } from "./data.js";
+import "./path-to-css.css";
+
+console.log("puppyList:", puppyList); // Add this before the App function
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
+
   return (
-    <>
-      <div></div>
-    </>
+    <div className="App">
+      <h1>Puppy List</h1>
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>;
+      })}
+    </div>
   );
 }
 
